a3e8566d66208d18ed29cdc41f04889aedb0c4b9
jbk2 <james@humanbenefits.com>
2025-02-17 12:26:55 +0100

    Initial scaffold of board in html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import './assets/stylesheets/styles.css'
+import board from './views/partials/_board.html'
+
+document.addEventListener("DOMContentLoaded", () => {
+  const humansBoard = document.getElementById('humans-board')
+  humansBoard.insertAdjacentHTML('beforeend', board);
+  humansBoard.querySelector('.board-title').innerText = "Human player's board"
+  
+  const computersBoard = document.getElementById('computers-board')
+  computersBoard.insertAdjacentHTML('beforeend', board);
+  computersBoard.querySelector('.board-title').innerText = "Computer player's board"
+
+
+})
+
 console.log("Hello This is a Javascript project. I am working");
