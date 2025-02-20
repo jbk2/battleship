8b6f18356694f8b7d7a68ce0c8ccab9bd39d4683
jbk2 <james@humanbenefits.com>
2025-02-20 19:01:41 +0100

    All tests passing with proceeMove separately implemented for human & computer

--- src/test/game.test.js
+++ src/test/game.test.js
@@ -53,95 +53,95 @@ describe('Game', () => {
         expect(computerPlayersA1Cell.attacked).toBe(true);
       })
 
-      // it('should not place the attack if it is not the activePlayer', () => {
-      //   const newGame = new Game();
-      //   newGame.setActivePlayer('computer');
-      //   expect(() => newGame.processHumanMove('a1'))
-      //     .toThrowError(`activePlayer is '${newGame.getActivePlayer()}', 
-      //     activePlayer must be 'human' for a 'human' to attack`);
-      // })
+      it('should not place the attack if it is not the activePlayer', () => {
+        const newGame = new Game();
+        newGame.setActivePlayer('computer');
+        expect(() => newGame.processHumanMove('a1'))
+          .toThrowError(`activePlayer is '${newGame.getActivePlayer()}', \
+          activePlayer must be 'human' for a 'human' to attack`);
+      })
       
-      // it('should toggle the active player after placing the attack', () => {
-      //   const newGame = new Game();
+      it('should toggle the active player after placing the attack', () => {
+        const newGame = new Game();
     
-      //   expect(newGame.getActivePlayer()).toBe('human');
-      //   newGame.processHumanMove('a1');
-      //   expect(newGame.getActivePlayer()).toBe('computer');
-      // })
+        expect(newGame.getActivePlayer()).toBe('human');
+        newGame.processHumanMove('a1');
+        expect(newGame.getActivePlayer()).toBe('computer');
+      })
 
-      // it("declares human the game winner if all computer's ships are sunk", () => {
-      //   const newGame = new Game();
-        
-      //   computerPlayersBoard.sinkFleet();
-      //   expect(newGame.processHumanMove('a1')).toEqual('Human wins');
-      // })
+      it("declares human the game winner if all computer's ships are sunk", () => {
+        const newGame = new Game();
+        const computerPlayersBoard = newGame.getComputerPlayer().getBoard();
+        computerPlayersBoard.sinkFleet();
+        expect(newGame.processHumanMove('a1')).toEqual('Human wins');
+      })
 
-      // it('should re-render the computers board', () => {
-      // })
+      it('should re-render the computers board', () => {
+      })
     })
   })
 
-  // describe('processComputerMove()', () => {
-  //   describe('a computer player attacking a human player', () => {
-  //     it("should place the attack on the computer player's board", () => {
-  //       const newGame = new Game();
-  //       const humanPlayersBoard = newGame.getHumanPlayer().getBoard();
-  //       newGame.setActivePlayer('computer');
-  //       const preAttackCells = Object.values(humanPlayersBoard.getGrid())
-  //         .flatMap(row => Object.values(row))
-  //         .filter(cell => cell.attacked);
-  //       expect(preAttackCells.length).toBe(0);
+  describe('processComputerMove()', () => {
+    describe('a computer player attacking a human player', () => {
+      it("should place the attack on the computer player's board", () => {
+        const newGame = new Game();
+        const humanPlayersBoard = newGame.getHumanPlayer().getBoard();
+        newGame.setActivePlayer('computer');
+        const preAttackCells = Object.values(humanPlayersBoard.getGrid())
+          .flatMap(row => Object.values(row))
+          .filter(cell => cell.attacked);
+        expect(preAttackCells.length).toBe(0);
 
-  //       newGame.processComputerMove();
-  //       const postAttackCells = Object.values(humanPlayersBoard.getGrid())
-  //         .flatMap(row => Object.values(row))
-  //         .filter(cell => cell.attacked);
-  //       expect(postAttackCells.length).toBe(1);
-  //     })
-  //   })
+        newGame.processComputerMove();
+        const postAttackCells = Object.values(humanPlayersBoard.getGrid())
+          .flatMap(row => Object.values(row))
+          .filter(cell => cell.attacked);
+        expect(postAttackCells.length).toBe(1);
+      })
+    })
 
-  //   it('should not place the attack if it is not the activePlayer', () => {
-  //     const newGame = new Game();
-  //     const humanPlayer = newGame.getHumanPlayer();
-  //     const computerPlayer = newGame.getComputerPlayer();
-  //     expect(() => {newGame.processComputerMove()})
-  //       .toThrowError(`activePlayer is '${newGame.getActivePlayer()}', 
-  //       activePlayer must be 'computer' for a 'computer' to attack`);
-  //   })
+    it('should not place the attack if it is not the activePlayer', () => {
+      const newGame = new Game();
+      const humanPlayer = newGame.getHumanPlayer();
+      const computerPlayer = newGame.getComputerPlayer();
+      expect(() => {newGame.processComputerMove()})
+        .toThrowError(`activePlayer is '${newGame.getActivePlayer()}', \
+        activePlayer must be 'computer' for a 'computer' to attack`);
+    })
 
-  //   it('should toggle the active player after placing the attack', () => {
-  //     const newGame = new Game();
-  //     const computerPlayer = newGame.getComputerPlayer();
-  //     newGame.setActivePlayer('computer');
-  //     expect(newGame.getActivePlayer()).toBe('computer');
-  //     newGame.processComputerMove();
-  //     expect(newGame.getActivePlayer()).toBe('human');
-  //   })
+    it('should toggle the active player after placing the attack', () => {
+      const newGame = new Game();
+      const computerPlayer = newGame.getComputerPlayer();
+      newGame.setActivePlayer('computer');
+      expect(newGame.getActivePlayer()).toBe('computer');
+      newGame.processComputerMove();
+      expect(newGame.getActivePlayer()).toBe('human');
+    })
 
-  //   it("declares computer the game winner if all humans's ships are sunk", () => {
-  //     const newGame = new Game();
-  //     const humanPlayer = newGame.getHumanPlayer();
-  //     const computerPlayer = newGame.getComputerPlayer();
-  //     const humanPlayersBoard = humanPlayer.getBoard();
-  //     humanPlayersBoard.sinkFleet();
-  //     newGame.setActivePlayer('computer')
-  //     expect(newGame.processComputerMove()).toEqual('Computer wins');
-  //   })
-  // })
+    it("declares computer the game winner if all humans's ships are sunk", () => {
+      const newGame = new Game();
+      const humanPlayer = newGame.getHumanPlayer();
+      const computerPlayer = newGame.getComputerPlayer();
+      const humanPlayersBoard = humanPlayer.getBoard();
+      humanPlayersBoard.sinkFleet();
+      newGame.setActivePlayer('computer')
+      expect(newGame.processComputerMove()).toEqual('Computer wins');
+    })
+  })
 
-  // describe('checkWin()', () => {
-  //   it("returns true if given board's fleet is sunk", () => {
-  //     const newGame = new Game();
-  //     const humanPlayersBoard = newGame.getHumanPlayer().getBoard();
-  //     humanPlayersBoard.sinkFleet();
-  //     expect(newGame.checkWin(humanPlayersBoard)).toBe(true)
-  //   })  
-  //   it("returns false if given board's fleet is not sunk", () => {
-  //     const newGame = new Game();
-  //     const humanPlayersBoard = newGame.getHumanPlayer().getBoard();
-  //     expect(newGame.checkWin(humanPlayersBoard)).toBe(false)
-  //   })  
+  describe('checkWin()', () => {
+    it("returns true if given board's fleet is sunk", () => {
+      const newGame = new Game();
+      const humanPlayersBoard = newGame.getHumanPlayer().getBoard();
+      humanPlayersBoard.sinkFleet();
+      expect(newGame.checkWin(humanPlayersBoard)).toBe(true)
+    })  
+    it("returns false if given board's fleet is not sunk", () => {
+      const newGame = new Game();
+      const humanPlayersBoard = newGame.getHumanPlayer().getBoard();
+      expect(newGame.checkWin(humanPlayersBoard)).toBe(false)
+    })  
 
-  // })
+  })
   
 })
